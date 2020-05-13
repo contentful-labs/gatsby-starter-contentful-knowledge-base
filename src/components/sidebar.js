2df56ade382986dfae2c30cfe7c728cb7f35da14
Cezar Sampaio <cezar.sampaio@contentful.com>
2020-05-13 15:41:38 +0200

    feat: create styles for the category articles page

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -1,18 +1,125 @@
-import React from 'react';
-import { Link } from 'gatsby';
+import React, { useState } from 'react';
+import { Link as GatsbyLink } from 'gatsby';
+import styled from '@emotion/styled';
+
+const Text = styled.p`
+  margin-bottom: 20px;
+
+  font-size: 14px;
+
+  &:last-child {
+    margin-bottom: 0;
+  }
+`;
+
+const Link = styled(GatsbyLink)`
+  text-decoration: none;
+  color: #536171;
+
+  &:hover,
+  &:focus {
+    text-decoration: underline;
+  }
+`;
+
+const DesktopNav = styled.nav`
+  display: none;
+
+  @media screen and (min-width: 769px) {
+    display: block;
+  }
+
+  @media print {
+    display: block;
+  }
+`;
+
+const MobileNav = styled.nav`
+  display: none;
+
+  background-color: #efefef;
+  border-radius: 3px;
+
+  @media screen and (max-width: 768px) {
+    display: block;
+  }
+`;
+
+const MobileMenuOpener = styled.button`
+  display: flex;
+  width: 100%;
+  align-items: center;
+  justify-content: space-between;
+
+  font-size: 14px;
+  padding: 16px;
+  margin-bottom: 0;
+
+  border: 0;
+  background: none;
+  -webkit-appearance: none;
+
+  &:after {
+    content: '';
+    height: 7px;
+    width: 7px;
+    margin-left: 8px;
+    transform: rotate(${(props) => (props.isOpened ? '45deg' : '-45deg')});
+    border-bottom: 2px solid currentcolor;
+    border-right: 2px solid currentcolor;
+    transition: all 0.25s ease 0s;
+  }
+`;
+
+const MobileMenuLinksContainer = styled.div`
+  padding: 16px;
+`;
 
 export default function Sidebar(props) {
+  const [isMobileMenuOpened, setIsMobileMenuOpened] = useState(false);
+
+  function toggleMobileMenu() {
+    setIsMobileMenuOpened((currValue) => !currValue);
+  }
+
   return (
-    <aside className="sidebar">
-      {props.data?.map(({ node }) => (
-        <div>
-          <div>
-            <p key={node.slug}>
-              <Link to={`/${node.slug}/`}>{node.name}</Link>
-            </p>
-          </div>
-        </div>
-      ))}
+    <aside>
+      <DesktopNav>
+        {props.data?.map(({ node }) => (
+          <Text key={node.slug}>
+            <Link
+              to={`/${node.slug}/`}
+              activeStyle={{ color: '#000', textDecoration: 'underline' }}
+            >
+              {node.name}
+            </Link>
+          </Text>
+        ))}
+      </DesktopNav>
+
+      <MobileNav>
+        <MobileMenuOpener
+          isOpened={isMobileMenuOpened}
+          onClick={toggleMobileMenu}
+        >
+          Topic
+        </MobileMenuOpener>
+
+        {isMobileMenuOpened && (
+          <MobileMenuLinksContainer>
+            {props.data?.map(({ node }) => (
+              <Text key={node.slug}>
+                <Link
+                  to={`/${node.slug}/`}
+                  activeStyle={{ color: '#000', textDecoration: 'underline' }}
+                >
+                  {node.name}
+                </Link>
+              </Text>
+            ))}
+          </MobileMenuLinksContainer>
+        )}
+      </MobileNav>
     </aside>
   );
 }
--- src/templates/section.js
+++ src/templates/section.js
@@ -1,34 +1,85 @@
 import React from 'react';
-import { graphql, Link } from 'gatsby';
+import { graphql } from 'gatsby';
+import styled from '@emotion/styled';
 import Layout from './layout';
 import Sidebar from '../components/sidebar';
+import WhiteContainer from '../components/white-container';
+import ArticleLink from '../components/article-link';
+
+const GridContainer = styled.div`
+  display: grid;
+  grid-template-rows: repeat(2, auto);
+  grid-template-columns: 245px 1fr;
+  column-gap: 32px;
+  align-items: start;
+  grid-template-areas "empty title-group" "sidebar articles";
+
+  @media screen and (max-width: 768px) {
+    display: block;
+  }
+`;
+
+const ArticleContainer = styled.div`
+  padding: 12px 32px;
+`;
+
+const CategoryTitle = styled.h1`
+  margin-bottom: 12px;
+
+  color: #2a3039;
+  font-size: 28px;
+  line-height: 1.5;
+  font-weight: 700;
+`;
+
+const CategoryDescription = styled.h2`
+  color: #536171;
+  font-size: 21px;
+  line-height: 1.5;
+  font-weight: 400;
+`;
+
+const TitleGroup = styled.div``;
+
+const Cell = styled.div`
+  grid-area: ${(props) => props.area};
+  margin-bottom: 32px;
+`;
 
 export default function Section(props) {
   return (
     <Layout>
-      <div className="content">
-        <Sidebar data={props.data?.categories?.edges} />
+      <GridContainer>
+        <Cell area="empty"></Cell>
 
-        <section>
-          <h1>{props.data?.category?.name}</h1>
+        <Cell area="title-group">
+          <TitleGroup>
+            <CategoryTitle>{props.data?.category?.name}</CategoryTitle>
+            <CategoryDescription>
+              The basics of ordering BurgerKing
+            </CategoryDescription>
+          </TitleGroup>
+        </Cell>
 
-          {props.data?.subCategories?.edges.map(({ node }) => (
-            <div key={node.slug}>
-              <h2>{node.name}</h2>
+        <Cell area="sidebar">
+          <Sidebar data={props.data?.categories?.edges} />
+        </Cell>
 
-              {node.articles?.map((article) => (
-                <p key={article.slug}>
-                  <Link
-                    to={`/${props.data?.category?.slug}/${node.slug}/${article.slug}/`}
-                  >
-                    {article.title}
-                  </Link>
-                </p>
-              ))}
-            </div>
-          ))}
-        </section>
-      </div>
+        <Cell area="articles">
+          <WhiteContainer>
+            <ArticleContainer>
+              {props.data?.subCategories?.edges.map(({ node }) =>
+                node.articles?.map((article) => (
+                  <ArticleLink
+                    url={`/${props.data?.category?.slug}/${node.slug}/${article.slug}/`}
+                    label={article.title}
+                  />
+                )),
+              )}
+            </ArticleContainer>
+          </WhiteContainer>
+        </Cell>
+      </GridContainer>
     </Layout>
   );
 }
