4f83fd95fb2cf4c57565211313c2d022f49cecbf
Cezar Sampaio <cezar.sampaio@contentful.com>
2020-05-22 14:56:23 +0200

    feat: add 404 page

--- gatsby-config.js
+++ gatsby-config.js
@@ -79,5 +79,6 @@ module.exports = {
         ],
       },
     },
+    'gatsby-plugin-netlify',
   ],
 };
---
+++ src/pages/404.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import styled from '@emotion/styled';
+import { Link as GatsbyLink } from 'gatsby';
+import Layout from '../templates/layout';
+
+const Title = styled.h1`
+  margin-bottom: 12px;
+
+  color: #2a3039;
+  font-weight: 700;
+  font-size: 28px;
+`;
+
+const SubTitle = styled.h3`
+  margin-bottom: 32px;
+
+  color: #536171;
+  font-weight: 400;
+  font-size: 21px;
+`;
+
+const Link = styled(GatsbyLink)`
+  color: #3072be;
+  text-decoration: none;
+
+  &:hover,
+  &:focus {
+    text-decoration: underline;
+  }
+`
+
+export default function Article() {
+  return (
+    <Layout withSearch={true}>
+      {/* <SEO
+        title={article.title}
+        description={article.description.description}
+        lang={article.locale}
+      /> */}
+
+      <Title>The page you are looking for can’t be found.</Title>
+      <SubTitle>Error code: 404</SubTitle>
+      <Link to="/">Return to home page</Link>
+    </Layout>
+  );
+}
