9c62ebcd03fed85122d25a70c30aec318f79542c
Cezar Sampaio <cezar.sampaio@contentful.com>
2020-05-25 16:35:58 +0200

    build: update npm packages version but not publish it

--- release.config.js
+++ release.config.js
@@ -5,12 +5,17 @@ module.exports = {
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
-    '@semantic-release/github',
     '@semantic-release/changelog',
+    [
+      '@semantic-release/npm',
+      {
+        npmPublish: false,
+      },
+    ],
     [
       '@semantic-release/git',
       {
-        assets: [],
+        assets: ['CHANGELOG.md', 'package.json', 'package-lock.json'],
       },
     ],
   ],
