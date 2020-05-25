582b4e0f414733dd7f741bcc780c73d2f0e09bc1
Cezar Sampaio <cezar.sampaio@contentful.com>
2020-05-25 15:50:29 +0200

    build: add semantic-release

---
+++ release.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  release: {
+    branches: ['master'],
+  },
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/github',
+    [
+      '@semantic-release/git',
+      {
+        assets: false,
+      },
+    ],
+  ],
+};
